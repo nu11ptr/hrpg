5ede2f7caeeb1d9e85d1e523270f86501909ec62
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2021-09-21 23:13:31 -0400

    Initial rust code

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use clap::{App, Arg};
+
+fn main() {
+    let app = App::new("Human Readable Parser Generator")
+        .version("v0.1.0")
+        .arg(Arg::with_name("config")
+            .short("c")
+            .long("config")
+            .takes_value(true)
+            .help("Configuration file specifying overrides to the default configuration"))
+        .arg(Arg::with_name("INPUT")
+            .required(true)
+            .index(1)
+            .help("The grammar file to parse"));
+
+    let matches = app.get_matches();
+    let config = matches.value_of("config");
+    let grammar = matches.value_of("INPUT");
+
+    println!("Grammar: {}", grammar.unwrap());
+    println!("Config: {}", config.unwrap_or("<N/A>"))
+}
