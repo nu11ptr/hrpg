db379002d981a6024b07ad84e91cb2a00957a715
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2021-09-26 00:20:32 -0400

    Refactor main plus fix grammar comments

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::fs;
 use clap::{App, Arg};
 
 use hrpg_core::{ast, process};
+use std::error::Error;
 
 fn main() {
     let app = App::new("Human Readable Parser Generator")
@@ -18,14 +19,22 @@ fn main() {
             .help("The grammar file to parse"));
 
     let matches = app.get_matches();
-    let config = matches.value_of("config");
-    let grammar = matches.value_of("INPUT");
+    let config_file = matches.value_of("config");
+    let input_file = matches.value_of("INPUT");
 
-    println!("Grammar: {}", grammar.unwrap());
-    println!("Config: {}", config.unwrap_or("<N/A>"));
+    match process_input(input_file.unwrap(), config_file) {
+        Ok(_) => (),
+        Err(err) => println!("An error occurred: {}", err),
+    }
+}
 
-    let data = fs::read_to_string(grammar.unwrap()).expect("File not found");
-    let g = ast::parse_hrpg(&data).unwrap();
+fn process_input(input_file: &str, config_file: Option<&str>) -> Result<(), Box<dyn Error>> {
+    println!("Grammar: {}", input_file);
+    println!("Config: {}\n", config_file.unwrap_or("<N/A>"));
+
+    let data = fs::read_to_string(input_file)?;
+    let g = ast::parse_hrpg(&data)?;
+
     println!("Original AST: {:?}\n", g);
 
     let mut proc = process::Process::new();
@@ -34,4 +43,6 @@ fn main() {
 
     println!("Tokens: {:?}", proc.token_names);
     println!("Errors: {:?}", proc.errors);
+
+    Ok(())
 }
