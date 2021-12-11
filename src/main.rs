a8fe79c8c0ecb239b068d2df8378574f0e600af6
Scott Meeuwsen <3615587+nu11ptr@users.noreply.github.com>
2021-12-11 16:01:37 -0500

    Update clap and args

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,48 @@
 use std::error::Error;
 use std::fs;
+use std::path::PathBuf;
+use std::str::FromStr;
 
-use clap::{AppSettings, Clap};
+use clap::Parser;
 
 use hrpg::ast::parse_hrpg;
 use hrpg::diagram::draw_diagram;
 use hrpg::transform::Transform;
 
 /// Human Readable Parser Generator
-#[derive(Clap)]
-#[clap(version = "v0.1.0")]
-#[clap(setting = AppSettings::ColoredHelp)]
-struct Options {
+#[derive(clap::Parser)]
+#[clap(author = "Scott Meeuwsen <smeeuwsen@gmail.com>", version = "v0.1.0")]
+struct Args {
     #[clap(subcommand)]
     sub_cmd: SubCommands,
 }
 
-#[derive(Clap)]
+#[derive(clap::Parser)]
 enum SubCommands {
     Build(Build),
     Draw(Draw),
 }
 
 /// Build lexer/parser from a grammar
-#[derive(Clap)]
+#[derive(clap::Parser)]
 struct Build {
     /// Configuration file specifying overrides to the default configuration
     #[clap(short, long)]
-    config_file: Option<String>,
+    config_file: Option<PathBuf>,
 
     /// The grammar file to build
-    input_file: String,
+    input_file: PathBuf,
 }
 
 /// Draw railroad/syntax diagram from a grammar
-#[derive(Clap)]
+#[derive(clap::Parser)]
 struct Draw {
     /// The grammar file to draw
-    input_file: String,
+    input_file: PathBuf,
 }
 
 fn main() {
-    let options = Options::parse();
+    let options = Args::parse();
 
     let result = match options.sub_cmd {
         SubCommands::Build(build) => process_build(&build),
@@ -50,15 +51,18 @@ fn main() {
     match result {
         Ok(Some(str)) => println!("{}", str),
         Ok(None) => (),
-        Err(err) => println!("An error occurred: {}", err),
+        Err(err) => eprintln!("An error occurred: {}", err),
     }
 }
 
 fn process_build(build: &Build) -> Result<Option<String>, Box<dyn Error>> {
-    println!("Grammar: {}", &build.input_file);
+    println!("Grammar: {:?}", &build.input_file);
     println!(
-        "Config: {}\n",
-        build.config_file.as_ref().unwrap_or(&"<N/A>".to_owned())
+        "Config: {:?}\n",
+        build
+            .config_file
+            .as_ref()
+            .unwrap_or(&PathBuf::from_str("N/A").unwrap())
     );
 
     let data = fs::read_to_string(&build.input_file)?;
@@ -76,7 +80,7 @@ fn process_build(build: &Build) -> Result<Option<String>, Box<dyn Error>> {
 }
 
 fn process_draw(draw: &Draw) -> Result<Option<String>, Box<dyn Error>> {
-    eprintln!("Grammar: {}", &draw.input_file);
+    eprintln!("Grammar: {:?}", &draw.input_file);
     let data = fs::read_to_string(&draw.input_file)?;
     let g = parse_hrpg(&data)?;
     let (g2, transform) = Transform::process(&g);
